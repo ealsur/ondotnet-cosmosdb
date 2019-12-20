e0758937cfd68f5164d85183311a7c347d57ae47
Matias Quaranta <maquaran@microsoft.com>
2019-12-20 10:54:49 -0800

    Improve on serializer

--- src/episode1/customserializer/TextJsonSerializer.cs
+++ src/episode1/customserializer/TextJsonSerializer.cs
@@ -7,25 +7,56 @@ namespace episode1
 {
     public class TextJsonSerializer : CosmosSerializer
     {
-        private JsonSerializerOptions options = new JsonSerializerOptions()
+        private static JsonSerializerOptions options = new JsonSerializerOptions()
         {
             IgnoreNullValues = true,
-            PropertyNameCaseInsensitive = true
+            PropertyNameCaseInsensitive = true,
+            WriteIndented = false, 
+            DefaultBufferSize = 1024
         };
 
         public override T FromStream<T>(Stream stream)
         {
             using (stream)
             {
-                return JsonSerializer.DeserializeAsync<T>(stream, this.options).GetAwaiter().GetResult();
+                if (typeof(Stream).IsAssignableFrom(typeof(T)))
+                {
+                    return (T)(object)stream;
+                }
+
+                ReadOnlySpan<byte> span;
+                MemoryStream memoryStream = stream as MemoryStream;
+                if (memoryStream != null)
+                {
+                    span = new ReadOnlySpan<byte>(memoryStream.ToArray());
+                }
+                else
+                {
+                    byte[] buffer = new byte[16 * 1024];
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        int read;
+                        while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            ms.Write(buffer, 0, read);
+                        }
+
+                        span = new ReadOnlySpan<byte>(ms.ToArray());
+                    }
+                    
+                }
+                
+                return JsonSerializer.Deserialize<T>(span, TextJsonSerializer.options);
             }
         }
 
         public override Stream ToStream<T>(T input)
         {
-            MemoryStream stream = new MemoryStream();
-            JsonSerializer.SerializeAsync(stream, input, this.options).GetAwaiter().GetResult();
-            return stream;
+            MemoryStream streamPayload = new MemoryStream();
+            using Utf8JsonWriter utf8JsonWriter = new Utf8JsonWriter(streamPayload);
+            JsonSerializer.Serialize<T>(utf8JsonWriter, input, TextJsonSerializer.options);
+            streamPayload.Position = 0;
+            return streamPayload;
         }
     }
 }
